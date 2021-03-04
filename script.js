82b457840113efeefedc16064d0c44336f18cb8a
Yuriy Lasygin <lasygin@gmail.com>
2021-03-04 11:08:01 +0300

    Redo structure + separation of concerns, change animations

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
+let tl = gsap.timeline();
+
+tl.from('.transition1', {
+  y: '-30%',
+  opacity: 0,
+  duration: 1,
+  ease: Power2.easeOut,
+});
+tl.from('.transition2', {
+  opacity: 0,
+  duration: .5,
+});
+tl.from('.transition3', {
+  opacity: 0,
+  duration: 1,
+  ease: Power2.easeOut,
+});
+
+const projectsLink = document.getElementById('projects-link');
+const meLink = document.getElementById('me-link');
+projectsLink.addEventListener('click', () => {
+  gsap.to(window, { duration: .5, scrollTo: '#projects' });
+});
+meLink.addEventListener('click', () => {
+  gsap.to(window, { duration: .5, scrollTo: '#me' });
+});
